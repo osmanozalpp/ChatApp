9752ad65e5be66836a2f2119676d9c484e9923b9
Berkaytln <berkay.talan852@gmail.com>
2024-11-29 20:39:38 +0300

    1st par completed - backend

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
+
+const app= express();
+const PORT= process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // (req.body'den) gelen istekleri json yükleriyle ayrıştırmak için 
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
+
+
+
+/*app.get("/",(req,res) => {
+//root route http://localhost:5000/
+    res.send("Hello world!!")
+   
+});*/
+
+
+
+app.listen(PORT,()=> {
+connectToMongoDB();
+console.log(`Server Running on port ${PORT}`)
+});
+
